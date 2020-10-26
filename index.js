41260e2a067f6b24f0ba1d37825c0a6f2ed055ed
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 15:08:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
-function sayHiToGrandma(stirng){
+function sayHiToGrandma(string){
   if(lowercase === string.toLowerCase()){
     return "I can't hear you!"
   }else if(uppercase === string.toUpperCase()) {
